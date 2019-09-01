f1662075437bcbe8b13d28bbdb7d91caf8da08e5
Sage Khanuja <sagekhanuja@Sages-MacBook-Pro.local>
2019-09-01 10:42:55 -0700

    more questions

---
+++ java_questions/src/clock_angle.java
@@ -0,0 +1,32 @@
+public class clock_angle {
+
+
+/*Given a clock time in hh:mm format, determine, to the nearest degree, the angle between the hour and the minute hands.*/
+    
+
+    private static int calcAngle(double h, double m) {
+        // validate the input
+        if (h <0 || m < 0 || h >12 || m > 60)
+            System.out.println("Wrong input");
+
+        if (h == 12)
+            h = 0;
+        if (m == 60)
+            m = 0;
+
+        // Calculate the angles moved by hour and minute hands
+        // with reference to 12:00
+        int hour_angle = (int)(0.5 * (h*60 + m));
+        int minute_angle = (int)(6*m);
+
+        // Find the difference between two angles
+        int angle = Math.abs(hour_angle - minute_angle);
+
+        // smaller angle of two possible angles
+        angle = Math.min(360-angle, angle);
+
+        return angle;
+    }
+
+
+}

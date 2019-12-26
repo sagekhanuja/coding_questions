ca2fee5fd62d293496ef70f0fbd3f4a9371e62bf
Sage K <sagek1@gmail.com>
2019-12-25 17:15:13 -0800

    javascript questions

---
+++ javascript_questions/lastletter.js
@@ -0,0 +1,10 @@
+/*
+Write a function called lastLetter that takes a string and returns the very last letter of that string
+*/
+
+function lastLetter(s){
+    return s.charAt(s.length - 1);
+}
+
+
+console.log(lastLetter("abcd"));
